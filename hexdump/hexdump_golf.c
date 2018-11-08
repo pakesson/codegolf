496a3119dc0571c828dcd861293d44d34e1f8a97
Philip Åkesson <philip.akesson@gmail.com>
2018-11-09 00:54:19 +0100

    Shave off a few more characters. Hexdump is down to 230 bytes.

--- hexdump/hexdump_golf.c
+++ hexdump/hexdump_golf.c
@@ -1,2 +1,1 @@
-#define s sprintf
-int main(int a,char*v[]){int c,n,t=0;FILE*p=fopen(v[1],"r");while(c!=EOF){char l[81]={0};s(l,"%08X ",t);for(n=0;n<16&&c!=EOF;++n){c=fgetc(p);s(l+9+n*3,"%02X",(unsigned)c);l[11+n*3]=' ';s(l+57+n,"%c",(c>31&&c<124)?c:'.');}puts(l);t+=16;}}
+int main(int a,char*v[]){int c,n,t=0;FILE*p=fopen(v[1],"r");while(c!=EOF){char l[81]={0};sprintf(l,"%08X ",t);for(n=0;n<16&&c!=EOF;++n){c=fgetc(p);sprintf(l+9+n*3,"%02X ",c);l[58+n]=(c>31&&c<124)?c:'.';}l[57]=' ';puts(l);t+=16;}}
