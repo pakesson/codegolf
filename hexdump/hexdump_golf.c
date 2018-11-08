8a272ea95ed03c899336b5f911aea73d740ea4fe
Philip Åkesson <philip.akesson@gmail.com>
2018-11-08 23:28:27 +0100

    Got rid of a bunch of unnecessary stuff in hexdump

--- hexdump/hexdump_golf.c
+++ hexdump/hexdump_golf.c
@@ -3,21 +3,20 @@
 
 int main(int a, char *v[])
 {
-    int c,t = 0;
-    FILE *p=fopen(v[1], "r");
+    int c,t=0;
+    FILE *p=fopen(v[1],"r");
     while (c!=EOF)
     {
         char l[81]={' '};
+        sprintf(l,"%08X ",t);
         for (int n=0;n<16&&c!=EOF;++n) {
             c = fgetc(p);
-            sprintf(l,"%08X",t);
-            sprintf(l+10+n*3,"%02X",(unsigned char)c);
-            l[10+n*3+2]=' ';
-            sprintf(l+60+n,(c>31&&c<124)?"%c":".",c);
-            l[60+n+1]=' '; 
+            sprintf(l+9+n*3,"%02X",(unsigned char)c);
+            l[11+n*3]=' ';
+            sprintf(l+57+n,(c>31&&c<124)?"%c":".",c);
+            l[58+n]=' ';
         }
-        l[8]=l[9]=l[58]=l[59]=' ';
-        printf("%s\n", l);
+        printf("%s\n",l);
         t+=16;
     }
     fclose(p);
